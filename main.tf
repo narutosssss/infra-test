8908389653c54049cc23eb14da614797661224d5
narutosssss <al2mza_kub@hotmail.com>
2019-02-14 13:58:57 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,28 @@
 
-data "aws_iam_role" "role" {
-    name = "MyRole"
+resource "aws_iam_role" "first_lambda_role" {
+  name = "first_lambda_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+  tags = {
+      tag-key = "tag-value"
+  }
 }
 
-resource "null_resource" "git_pull" {
-  # provisioner "local-exec" {
-  #   command = "git clone https://github.com/ifew/aws-lambda-proxy-function.git project_workspace"
-  # } 
-  # provisioner "local-exec" {
-  #   command = "cd project_workspace/test/aws-lambda-function.Tests && dotnet test"
-  #   interpreter = ["/bin/sh", "-c"]
-  # } 
-
-  # provisioner "local-exec" {
-  #   command           = "cd project_workspace/src/aws-lambda-function && dotnet publish -c Release"
-  #   interpreter       = ["/bin/sh", "-c"]
-  # }
-}
 
 #deploy lambda & rest api
 module "list_profile" {
@@ -27,8 +33,8 @@ module "list_profile" {
   runtime                 = "${module.global_variables.runtime}"
   source_dir              = "project_workspace/src/aws-lambda-function/bin/Release/netcoreapp2.1/publish"
   output_path             = "project_workspace/deployment.zip"
-  role_name               = "${data.aws_iam_role.role.arn}"
-  publish_version         = false
+  role_name               = "${aws_iam_role.first_lambda_role.arn}"
+  publish_version         = true
   #api
   api_name                = "my_api_name"
   resource_name           = "test"
